6e95820151ee7dea71252e5b43ac8371a5d5921b
Stephanie Green <steph.green44@gmail.com>
2020-10-22 06:54:13 -0600

    update form return

--- client/components/AddShipForm.js
+++ client/components/AddShipForm.js
@@ -3,10 +3,18 @@ import React from 'react';
 export class AddShipForm extends React.Component {
     constructor(props) {
         super(props)
-        // add State ??
+        this.state = {entry: 'Add a ship here'}
     }
 
     render() {
-        
+        return (
+            <form>
+                <input 
+                    onChange={HANDLE THE INPUT}
+                    placeholder={this.state.entry}
+                />
+                <button>Add a Ship!</button>
+            </form>
+        )
     }
 }
