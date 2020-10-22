0014d09e123ea7eb7cf4b367888d2d4b1523a0d1
Stephanie Green <steph.green44@gmail.com>
2020-10-22 06:34:38 -0600

    add SpaceShip component

---
+++ client/components/SpaceShips.js
@@ -0,0 +1,16 @@
+const SpaceShips = (props) => {
+    return (
+        <ul>
+        {props.spaceships}
+        </ul>
+    )
+}
+
+// Example Ship
+// {
+//     id: 1,
+//     name: "SS Anne",
+//     color: "blue"
+//   },
+
+// remember to pass props in
